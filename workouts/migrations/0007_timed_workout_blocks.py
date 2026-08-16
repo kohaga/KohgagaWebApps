d8954f69373b077c078060788ea14be5aef84051
Kovács-Hagyó Gábor <46133020+kohaga@users.noreply.github.com>
2026-08-16 10:21:11 +0200

    Backfill mobility and stretching durations

--- workouts/migrations/0007_timed_workout_blocks.py
+++ workouts/migrations/0007_timed_workout_blocks.py
@@ -1,6 +1,14 @@
 from django.db import migrations, models
 
 
+def set_default_timed_exercise_duration(apps, schema_editor):
+    Exercise = apps.get_model("workouts", "Exercise")
+    Exercise.objects.filter(
+        movement_pattern__in=["mobility", "stretching"],
+        default_duration_seconds__isnull=True,
+    ).update(default_duration_seconds=60)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -31,5 +39,9 @@ class Migration(migrations.Migration):
             model_name="workoutsessionexercise",
             name="target_duration_seconds",
             field=models.PositiveIntegerField(blank=True, null=True),
+        ),
+        migrations.RunPython(
+            set_default_timed_exercise_duration,
+            migrations.RunPython.noop,
         ),
     ]
